ab7110da2b2e53dd47f72b181561a52dd627d317
devdonatti <mdonattiprogramacion@gmail.com>
2025-03-07 18:25:16 -0300

    intentando conectar front y back

--- vite-project/src/components/buyNowModal/BuyNowModal.jsx
+++ vite-project/src/components/buyNowModal/BuyNowModal.jsx
@@ -34,7 +34,7 @@ const BuyNowModal = ({ addressInfo, setAddressInfo, buyNowFunction }) => {
 
       // Llamada al backend para crear la preferencia
       const response = await axios.post(
-        "https://ecommerce-smile-vercel-git-main-devdonattis-projects.vercel.app/create_preference_cart",
+        "https://ecommerce-smile-vercel-git-main-devdonattis-projects.vercel.app/api/create_preference_cart",
         { cartItems: items }
       );
 
